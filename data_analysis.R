624e95787a3cbcaa1f8ac7c91f785ec55d2cd368
Guillem Bonet <guillem.bonet@gmail.com>
2024-04-04 19:34:11 +0200

    Mean word length and random baseline
    Signed-off-by: Guillem Bonet <guillem.bonet@gmail.com>

--- data_analysis.R
+++ data_analysis.R
@@ -3,6 +3,8 @@ csv_files <- list.files(pattern = "^output_.*\\.csv$")
 
 # Initialize a vector to store all p-values
 all_p_values <- c()
+mwl <- c()
+rb <- c()
 
 # Iterate over each CSV file
 for (file in csv_files) {
@@ -19,8 +21,14 @@ for (file in csv_files) {
   # Store the p-value for the correlation test
   all_p_values <- c(all_p_values, result$p.value)
   
-  # Print the p-value for the correlation test
+  # Calculate weighted sum of lengths
+  weighted_sum <- sum(data$Frequency * data$Length)
+  total_frequency <- sum(data$Frequency)
+  mean_length <- weighted_sum / total_frequency
+  mwl <- c(mwl, mean_length)
   
+  length_sum <- sum(data$Length)
+  rb <- c(rb, length_sum/nrow(data))
 }
 
 # Apply Holm-Bonferroni correction to all p-values
@@ -30,5 +38,7 @@ for (i in seq_along(csv_files)) {
   cat("Correlation test p-value for file:", csv_files[i], "\n")
   cat("Original p-value:", all_p_values[i], "\n")
   cat("Holm-Bonferroni corrected p-value:", holm_corrected_p_values[i], "\n")
+  cat("Mean word length:", mwl[i], "\n")
+  cat("Random baseline:", rb[i], "\n")
   cat("\n")
 }

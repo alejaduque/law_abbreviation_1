e85dfebf69c685854bec86d7137cbb807f25378c
Guillem Bonet <guillem.bonet@gmail.com>
2024-04-06 17:46:43 +0200

    more calculations
    Signed-off-by: Guillem Bonet <guillem.bonet@gmail.com>

--- data_analysis.R
+++ data_analysis.R
@@ -5,13 +5,15 @@ csv_files <- list.files(pattern = "^output_.*\\.csv$")
 all_p_values <- c()
 mwl <- c()
 rb <- c()
+mb <- c()
+do <- c()
+os <- c()
 
 # Iterate over each CSV file
 for (file in csv_files) {
   # Read data from CSV file
   data <- read.csv(file)
   
-  # Assuming your CSV file has columns named "Length" and "Frequency"
   lengths <- data$Length
   frequencies <- data$Frequency
   
@@ -22,13 +24,24 @@ for (file in csv_files) {
   all_p_values <- c(all_p_values, result$p.value)
   
   # Calculate weighted sum of lengths
-  weighted_sum <- sum(data$Frequency * data$Length)
   total_frequency <- sum(data$Frequency)
-  mean_length <- weighted_sum / total_frequency
+  mean_length <- sum(data$Frequency/total_frequency * data$Length)
   mwl <- c(mwl, mean_length)
   
+  # Calculate random baseline
   length_sum <- sum(data$Length)
-  rb <- c(rb, length_sum/nrow(data))
+  random_b <- length_sum/nrow(data)
+  rb <- c(rb, random_b)
+  
+  # Minimum baseline
+  sorted_freqs <- sort(as.array(data$Frequency), decreasing = TRUE)
+  sorted_lengths <- sort(as.array(data$Length), decreasing = FALSE)
+  min_b <- sum(sorted_freqs/total_frequency * sorted_lengths)
+  mb <- c(mb, min_b)
+  
+  # Degree of optimality and optimality score
+  do <- c(do, min_b/mean_length)
+  os <- c(os, (random_b-mean_length)/(random_b-min_b))
 }
 
 # Apply Holm-Bonferroni correction to all p-values
@@ -40,5 +53,8 @@ for (i in seq_along(csv_files)) {
   cat("Holm-Bonferroni corrected p-value:", holm_corrected_p_values[i], "\n")
   cat("Mean word length:", mwl[i], "\n")
   cat("Random baseline:", rb[i], "\n")
+  cat("Minimum baseline:", mb[i], "\n")
+  cat("Degree of optimality:", do[i], "\n")
+  cat("Optimality score:", os[i], "\n")
   cat("\n")
 }
